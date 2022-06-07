2a415351a07b52401fb42e61f0acaf12dc7fdc9b
Chris Paggen <cpaggen@gmail.com>
2022-06-07 21:11:08 +0200

    deploying Ubuntu

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,7 +1,44 @@
 provider "aws" {
-  access_key = var.accessKey
-  secret_key = var.secretKey
-  region = "us-east-1"
+  region = "eu-central-1"
+  # credentials come from ~/.aws/credentials (AWS CLI)
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "cpaggen-bucket-one"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aesbucket1" {
+  bucket = aws_s3_bucket.bucket1.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+#locking part
+
+resource "aws_dynamodb_table" "tf_remote_state_locking" {
+  hash_key = "LockID"
+  name     = "cpaggen-tfstate"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "cpaggen-terraform-state"
+    key            = "index"
+    dynamodb_table = "cpaggen-tfstate"
+    region         = "eu-central-1"
+  }
 }
 
 resource "aws_vpc" "terraformvpc1" {
@@ -13,16 +50,16 @@ resource "aws_vpc" "terraformvpc1" {
   }
 }
 
-resource "aws_subnet" "first"{
-  cidr_block = "10.10.1.0/24"
-  vpc_id     = aws_vpc.terraformvpc1.id
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "first" {
+  cidr_block        = "10.10.1.0/24"
+  vpc_id            = aws_vpc.terraformvpc1.id
+  availability_zone = "eu-central-1a"
 }
 
-resource "aws_subnet" "second"{
-  cidr_block = "10.10.2.0/24"
-  vpc_id     = aws_vpc.terraformvpc1.id
-  availability_zone = "us-east-1b"
+resource "aws_subnet" "second" {
+  cidr_block        = "10.10.2.0/24"
+  vpc_id            = aws_vpc.terraformvpc1.id
+  availability_zone = "eu-central-1b"
 }
 
 resource "aws_route_table" "mgmt-rt" {
@@ -41,45 +78,71 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "mgmt-default" {
-  route_table_id = aws_route_table.mgmt-rt.id
+  route_table_id         = aws_route_table.mgmt-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
   depends_on = [
     aws_route_table.mgmt-rt,
     aws_internet_gateway.igw
   ]
 }
 
-resource "aws_route_table_association" "inet-assoc1" {
-  subnet_id = aws_subnet.first.id
+resource "aws_main_route_table_association" "main-rt" {
+  vpc_id         = aws_vpc.terraformvpc1.id
   route_table_id = aws_route_table.mgmt-rt.id
 }
 
-resource "aws_route_table_association" "inet-assoc2" {
-  subnet_id = aws_subnet.second.id
-  route_table_id = aws_route_table.mgmt-rt.id
+#resource "aws_route_table_association" "inet-assoc1" {
+#  subnet_id = aws_subnet.first.id
+#  route_table_id = aws_route_table.mgmt-rt.id
+#}
+#
+#resource "aws_route_table_association" "inet-assoc2" {
+#  subnet_id = aws_subnet.second.id
+#  route_table_id = aws_route_table.mgmt-rt.id
+#}
+
+locals {
+  rulesmap = {
+    "HTTP" = {
+      port        = 80,
+      cidr_blocks = ["0.0.0.0/0"],
+    }
+    "SSH" = {
+      port        = 22,
+      cidr_blocks = ["0.0.0.0/0"],
+    },
+    "BLASTR" = {
+      port        = 8888,
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
 
 resource "aws_security_group" "cpaggen-sg" {
   vpc_id = aws_subnet.first.vpc_id
-  
-  ingress {
+
+  dynamic "ingress" {
+    for_each = local.rulesmap
+    content {
+      description = ingress.key # HTTP or SSH
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
   tags = {
     Name = "cpaggen-default"
   }
-
 }
 
 data "aws_ami" "amazon_linux" {
@@ -94,63 +157,88 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "ec2_instance_one" {
   associate_public_ip_address = true
-  ami = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  key_name               = "virginia-keypair-one"
-  vpc_security_group_ids = [aws_security_group.cpaggen-sg.id]
-  subnet_id              = aws_subnet.first.id
+  ami                         = "ami-02584c1c9d05efa69" // data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = "frankfurt-keypair-one"
+  vpc_security_group_ids      = [aws_security_group.cpaggen-sg.id]
+  subnet_id                   = aws_subnet.first.id
   connection {
-      agent       = false
-      host        = self.public_ip
-      private_key = file("virginia-keypair-one.pem")
-      type        = "ssh"
-      user        = "ec2-user"
-    }
+    agent       = false
+    host        = self.public_ip
+    private_key = file("frankfurt-keypair-one.pem")
+    type        = "ssh"
+    user        = "ubuntu"
+  }
+
+  provisioner "file" {
+    source      = "nginx-playbook.yaml"
+    destination = "/home/ubuntu/nginx-playbook.yaml"
+  }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      "sleep 10",
-   ]
+      "curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -",
+      "sudo apt-add-repository \"deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main\"",
+      "sudo apt-get update -y && sudo apt-get install terraform -y",
+      "sudo apt install software-properties-common -y",
+      "sudo add-apt-repository ppa:deadsnakes/ppa -y",
+      "sudo apt install python3.7 python3-venv python3.7-venv python3-pip -y",
+      "python3.7 -m venv py37-venv",
+      "source ./py37-venv/bin/activate",
+      "pip3 install --upgrade pip",
+      "pip3 install blastradius graphviz",
+      "/home/ubuntu/.local/bin/blast-radius --serve --port 8888 &"
+    ]
   }
   tags = {
     Terraform   = "true"
     Environment = "dev"
-    Name = "cpaggen-one"
+    Name        = "cpaggen-one"
   }
 }
 
 resource "aws_instance" "ec2_instance_two" {
   associate_public_ip_address = true
-  ami = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  key_name               = "virginia-keypair-one"
-  vpc_security_group_ids = [aws_security_group.cpaggen-sg.id]
-  subnet_id              = aws_subnet.second.id
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = "frankfurt-keypair-one"
+  vpc_security_group_ids      = [aws_security_group.cpaggen-sg.id]
+  subnet_id                   = aws_subnet.second.id
   connection {
-      agent       = false
-      host        = self.public_ip
-      private_key = file("virginia-keypair-one.pem")
-      type        = "ssh"
-      user        = "ec2-user"
-    }
+    agent       = false
+    host        = self.public_ip
+    private_key = file("frankfurt-keypair-one.pem")
+    type        = "ssh"
+    user        = "ec2-user"
+  }
+
+  provisioner "file" {
+    source      = "nginx-playbook.yaml"
+    destination = "/home/ec2-user/nginx-playbook.yaml"
+  }
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
       "sleep 10",
-   ]
- }
- 
+      # "sudo yum install -y amazon-linux-extras",
+      # "sudo amazon-linux-extras install ansible2 -y",
+      # "sleep 5",
+      # "ansible-playbook /home/ec2-user/nginx-playbook.yaml"
+    ]
+
+
+  }
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
-    Name = "cpaggen-two"
+    Name        = "cpaggen-two"
   }
 }
 
 output "vpc-id" {
-    value = aws_vpc.terraformvpc1.id
+  value = aws_vpc.terraformvpc1.id
 }
 
 output "ec1-public-ip" {
@@ -161,3 +249,6 @@ output "ec2-public-ip" {
   value = aws_instance.ec2_instance_two.public_ip
 }
 
+output "bucket-name_b1" {
+  value = aws_s3_bucket.bucket1.bucket
+}
